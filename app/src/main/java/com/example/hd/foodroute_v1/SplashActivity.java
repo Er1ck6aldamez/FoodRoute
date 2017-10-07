18b78f6918ec3b87fcceda65eea916351f7720aa
ricardovilla92 <rikrdovilla160492@gmail.com>
2017-10-06 19:03:59 -0600

    modificacion de splash

--- app/src/main/java/com/example/hd/foodroute_v1/SplashActivity.java
+++ app/src/main/java/com/example/hd/foodroute_v1/SplashActivity.java
@@ -2,6 +2,7 @@ package com.example.hd.foodroute_v1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,12 +11,23 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class SplashActivity extends AppCompatActivity{
+    private final int DURACION_SPLASH = 1000; // 3 segundos
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, Principal.class);
-        startActivity(intent);
-        finish();
+        // Tenemos una plantilla llamada splash.xml donde mostraremos la información que queramos (logotipo, etc.)
+        //setContentView(R.drawable.splash);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
+                Intent intent = new Intent(SplashActivity.this, Principal.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+
     }
 }
