b2b4af9a46333b1224d2732d11e81a943ed8a9fe
Er1ck6aldamez <ebalmore.gs@gmail.com>
2017-10-19 23:46:11 -0600

    intent

--- app/src/main/java/com/example/hd/foodroute_v1/Buscar.java
+++ app/src/main/java/com/example/hd/foodroute_v1/Buscar.java
@@ -1,15 +1,30 @@
 package com.example.hd.foodroute_v1;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Buscar extends AppCompatActivity {
+
+    private Button btnBuscar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar);
+
+        btnBuscar = (Button) findViewById(R.id.btnBuscar);
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent res = new Intent(Buscar.this, Resultados.class);
+                startActivity(res);
+            }
+        });
+
 
 
         //creacion de la fecha atras
